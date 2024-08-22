56f597b5b6423606cfcc3319dc23c84f1e82b818
Harpinder Sandhu <harpindersandhu18@Gmail.com>
2024-08-21 18:14:14 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'favourites',
+  webDir: 'www'
+};
+
+export default config;
