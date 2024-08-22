1544f499860c9bc9819fa03bc1aadcc10f8fb9a6
Harpinder Sandhu <harpindersandhu18@Gmail.com>
2024-08-21 18:42:18 -0600

    Using lists and cards

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
+  // BOOKS ARRAY
+  books = [
+    {
+      id: 1,
+      title: 'Harry Potter',
+      genre: 'Fiction',
+      description: 'A short descripton',
+    },
+    {
+      id: 2,
+      title: 'Harry Potter 2',
+      genre: 'Fiction',
+      description: 'A short descripton',
+    },
+  ];
   constructor() {}
-
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ItemReorderEventDetail } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -9,4 +10,50 @@ export class Tab2Page {
 
   constructor() {}
 
+
+  songs = [
+    {
+      title:' Take On Me',
+      artist: 'Wow',
+      album: 'Take On Me',
+    },
+    {
+      title:' Wow',
+      artist: 'Wow',
+      album: 'Take On Me',
+    },
+    {
+      title:' Hello',
+      artist: 'Wow',
+      album: 'Take On Me',
+    },
+    {
+      title:' Mind on you',
+      artist: 'Wow',
+      album: 'Take On Me',
+    },
+    {
+      title:' Go!',
+      artist: 'Wow',
+      album: 'Take On Me',
+    },
+  ]
+  public isDisabled = true;
+
+
+  handleReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log(ev);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
+  }
+
+  toggleReorder() {
+    this.isDisabled = !this.isDisabled;
+  }
 }
+
